3845bc00025759c0f6b1692167ca1ed5a4a92f71
Valentin <valentin.linhardt@gmail.com>
2025-06-05 18:00:30 +0200

    path für instance files hinzugefügt

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('patient/<str:orthancId>/series', views.patient_series, name='patient_series'),
     path('study/<str:orthancId>/series', views.study_series, name='study_series'),
     path('instance/<str:orthancId>/preview', views.instance_preview, name='instance_preview'),
+    path('instance/<str:orthancId>/file', views.instance_file, name='instance_file')
 
 ]
